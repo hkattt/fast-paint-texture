bb48453e340eb9d8b4708eff2c381ddd294883cc
Hugo Kat <u7286091@anu.edu.au>
2024-05-29 14:28:06 +1000

    Adding final documentation and fixing texture images

--- include/paint.hpp
+++ include/paint.hpp
@@ -32,7 +32,6 @@ class FastPaintTexture {
         Texture *opacity_texture;
         
         /**
-         * TODO: Update
          * Paints a layer onto the canvas.
          * 
          * Implements the paintLayer psuedo-code from Painterly Rendering with Curved Brush 
@@ -47,24 +46,74 @@ class FastPaintTexture {
 
         float compose_height(float stroke_height, float stroke_opacity, float current_height);
 
+        /**
+         * Renders a stroke onto the canvas and height map
+         * 
+         * @param canvas: Canvas to render the stroke onto 
+         * @param height_map: Height map to render the stroke onto
+         * @param stroke: Stroke to render 
+         * @param mask: Anti-aliased circle kernel used to render the stroke
+        */
         void render_stroke(RGBImage *canvas, GrayImage *height_map, Stroke *stroke, AntiAliasedCircle *mask);
 
+        /**
+         * Renders a stroke point onto canvas and height map
+         * 
+         * @param canvas: Canvas to render the stroke onto 
+         * @param height_map: Height map to render the stroke onto
+         * @param stroke: Stroke to render 
+         * @param x: x-coordinate of the point
+         * @param y: y-coordinate of the point
+         * @param mask: Anti-aliased circle kernel used to render the stroke point
+        */
         void render_stroke_point(RGBImage *canvas, GrayImage *height_map, Stroke *stroke, int x, int y, AntiAliasedCircle *mask);
 
+        /**
+         * Renders a stroke line onto the canvas and height map
+         * 
+         * @param canvas: Canvas to render the stroke onto 
+         * @param height_map: Height map to render the stroke onto
+         * @param stroke: Stroke to render 
+         * @param x1: x-coordinate of the start point
+         * @param y1: y-coordinate of the start point
+         * @param x2: x-coordiante of the end point
+         * @param y2: y-coordiante of the end point
+         * @param mask: Anti-aliased circle kernel used to render the stroke
+        */
         void render_stroke_line(RGBImage *canvas, GrayImage *height_map, Stroke *stroke, int x1, int y1, int x2, int y2, AntiAliasedCircle *mask);
-
+        
+        /**
+         * Paints an image onto the canvas and height map
+         * 
+         * Implements the paint psuedo-code from Painterly Rendering with Curved Brush Strokes of Multiple Sizes by Aaron Hertzmann. 
+         * However, it also renders to a height map which is used to perform shading.
+         * 
+         * @return: Tuple containing the painted canvas and height map
+        */
         std::tuple<RGBImage*, GrayImage*> paint();
-
+        
+        /**
+         * Textures a painted image using its height map. 
+         * 
+         * @param image: Painted image to texture
+         * @param height_map: Height map of the painted image
+         * @param shader: Shader to use for lighting
+         * @param view_pos: View/eye position
+         * @param lights: Lights in the scene
+         * 
+         * @return: The textured canvas
+        */
         RGBImage *texture(RGBImage *image, GrayImage *height_map, Shader *shader, Vector3f view_pos, std::vector<Light> lights);
 
     public:
         /**
-         * TODO: Update?
          * Constructor for Paint.
          * 
          * @param width: The width of the rasterizer window.
          * @param height: The height of the rasterizer window.
          * @param source_image: The input image to be painted.
+         * @param height_texture: Height texture for the brush strokes
+         * @param opacity_texture: Opacity texture for the brush strokes
         */
         FastPaintTexture(int width, int height, cv::Mat source_image, Texture *height_texture, Texture *opacity_texture);
 
@@ -82,5 +131,10 @@ class FastPaintTexture {
             return this->source_image;
         }
 
+        /**
+         * Implemented the Fast Paint Texture described by Aaron Hertzmann in Fast Paint Texture.
+         * 
+         * @return: Tuple containing the textured painted image, painted image, and height map.
+        */
         std::tuple<RGBImage*, RGBImage*, GrayImage*> fast_paint_texture(Shader *shader);
 };
